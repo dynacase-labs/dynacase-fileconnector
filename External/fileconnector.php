9ab6d71376dee26364182a66b7d15db47eecc01e
Charles Bonnissent <charles.bonnissent@anakeen.com>
2013-02-01 16:51:39 +0100

    Convert to dynacase 3.2.5 (closes #3441)

--- External/fileconnector.php
+++ External/fileconnector.php
@@ -10,31 +10,25 @@ function getFilesAttr($dbaccess, $famid, $name)
     
     if (!$famid) return (_("family must be selected before"));
     $doc = createDoc($dbaccess, $famid, false);
-    // internal attributes
-    $ti = array(
-        "title" => _("doctitle") ,
-        "revdate" => _("revdate") ,
-        "revision" => _("revision") ,
-        "owner" => _("owner") ,
-        "state" => _("state")
-    );
     
     $tr = array();
     $pattern = preg_quote($name);
     $tinter = $doc->GetFileAttributes();
     while (list($k, $v) = each($tinter)) {
-        if (($name == "") || (preg_match("/$pattern/i", $v->labelText, $reg))) $tr[] = array(
-            $v->labelText,
-            $v->id,
-            $v->labelText
-        );
+        if (($name == "") || (preg_match("/$pattern/i", $v->labelText, $reg))) {
+            $tr[] = array(
+                $v->labelText,
+                $v->id,
+                $v->labelText
+            );
+        }
     }
     return $tr;
 }
 
 function getFamiliesWithFile($dbaccess, $fam)
 {
-    include_once ("EXTERNALS/fdl.php");
+    include_once "EXTERNALS/fdl.php";
     $tr = lfamilies($dbaccess, $fam);
     foreach ($tr as $k => $v) {
         $fd = new_Doc($dbaccess, $v[1], false, false);
@@ -69,4 +63,3 @@ function listProtocols()
     }
     return implode($p, ",");
 }
-?>
